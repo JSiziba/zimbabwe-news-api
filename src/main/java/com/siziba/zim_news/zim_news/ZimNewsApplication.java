553c029dbae92e4f9bf5835a462008984bc7d6d0
Johnson Andrew Siziba <sizibajohnsona@gmail.com>
2023-10-20 11:28:04 +0200

    Initial commit

---
+++ src/main/java/com/siziba/zim_news/zim_news/ZimNewsApplication.java
@@ -0,0 +1,13 @@
+package com.siziba.zim_news.zim_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimNewsApplication.class, args);
+	}
+
+}
